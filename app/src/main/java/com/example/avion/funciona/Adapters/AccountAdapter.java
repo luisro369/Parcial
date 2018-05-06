994c3e4ec60f57fe71fbfc1a4e243384bcb70b19
carme <00029613@uca.edu.sv>
2018-05-06 02:23:23 -0600

    No me funciona el adapter para accounts :(

--- app/src/main/java/com/example/avion/funciona/Adapters/AccountAdapter.java
+++ app/src/main/java/com/example/avion/funciona/Adapters/AccountAdapter.java
@@ -5,23 +5,26 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.example.avion.funciona.Entities.Account;
 import com.example.avion.funciona.R;
 
 import java.util.ArrayList;
 
-public class AccountAdapter {
+public class AccountAdapter extends RecyclerView.Adapter<AccountViewHolder>{
+
     ArrayList<Account> accounts_list;
 
-    public AccountAdapter(ArrayList<Account>accounts_list){this.accounts_list= accounts_list;}
+    //public AccountAdapter(ArrayList<Account> accounts_list){this.accounts_list= accounts_list;}
+
+    public AccountAdapter(ArrayList<Account> accounts_list) { this.accounts_list= accounts_list;
+    }
 
     @NonNull
     @Override
     public AccountViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_account, parent, false);
-        return new AccountViewHolder(view);
+        View account_view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_account, parent, false);
+        return new AccountViewHolder(account_view);
     }
 
 
@@ -32,12 +35,12 @@ public class AccountAdapter {
         holder.account_initial.setText(accounts_list.get(position).getInitial());
         holder.account_in.setText(accounts_list.get(position).getMoneyin());
         holder.account_off.setText(accounts_list.get(position).getMoneyoff());
-
-
     }
 
     @Override
-    public int getItemCount(){return accounts_list.size();}
+    public int getItemCount() {
+        return accounts_list.size();
+    }
 
 
 }
